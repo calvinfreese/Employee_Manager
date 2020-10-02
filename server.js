338388ba0dc4191113932c40ce48a94b3314d947
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-01 21:04:02 -0500

    added inquirer prompt with switch case to run functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const figlet = require('figlet');
 
+
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -15,12 +16,6 @@ var connection = mysql.createConnection({
     password: "",
     database: "employee_db"
   });
-
-
-
-
-
-
 
 
 
@@ -40,3 +35,90 @@ var connection = mysql.createConnection({
 
     
   });
+
+  function start() {
+    inquirer
+      .prompt({
+        type: "list",
+        message: "Would you like to do?",
+        choices: [
+          "View all employees",
+          "View all roles",
+          "View all departments",
+          "Add an employee",
+          "Add a role",
+          "Add a department",
+          "Update employee role",
+        ],
+        name: "choice"
+      })
+      .then(function (answer) {
+        switch (answer.choice) {
+          case "View all employees":
+            allEmployees();
+            break;
+          case "View all roles":
+            allRoles()
+            break;
+          case "View all departments":
+            allDeptartments();
+            break;
+          case "Add an employee":
+            addEmployee();
+            break;
+          case "Add a role":
+            addRole();
+            break;
+          case "Add a department":
+            addDepartment();
+            break;
+          case "Update employee role":
+            updateEmployee();
+            break;
+        }
+      });
+  }
+
+
+function allEmployees() {
+  connection.query("SELECT * FROM employee", function(err, res){
+    if (err) throw err;
+    console.log(res);
+    start();
+  });
+}
+
+function allRoles() {
+  connection.query("SELECT * FROM role", function(err, res){
+    if (err) throw err;
+    console.log(res);
+    start();
+  });
+}
+
+function allDeptartments() {
+  connection.query("SELECT * FROM department", function(err, res){
+    if (err) throw err;
+    console.log(res);
+    start();
+  });
+}
+
+// function addEmployee() {
+
+// }
+
+// function addRole() {
+
+// }
+
+// function addDepartment() {
+
+// }
+
+// function updateEmployee() {
+
+// }
+
+  
+
