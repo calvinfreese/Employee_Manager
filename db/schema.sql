332606e2b6bd2d3e0b63cbd43a062076c27ac07b
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-04 22:15:17 -0500

    removed IF NOT EXISTS statements

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,20 @@
+DROP DATABASE employee_db;
 CREATE DATABASE IF NOT EXISTS employee_db;
 
 USE employee_db;
 
-CREATE TABLE IF NOT EXISTS employee (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS role (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DEC(20,2) NOT NULL,
@@ -20,10 +22,13 @@ CREATE TABLE IF NOT EXISTS role (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS department (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+
 
 
